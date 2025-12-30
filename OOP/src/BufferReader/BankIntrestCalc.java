ca0900342e69e17cdde10cc3b4aec476542859ad
rishi-18-pvt <your-email@example.com>
2025-12-30 13:05:26 +0530

    BufferReader

---
+++ OOP/src/BufferReader/BankIntrestCalc.java
@@ -0,0 +1,32 @@
+package BufferReader;
+import java.io. InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.sql.Time;
+
+
+public class BankIntrestCalc {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter Name: ");
+        String Name = br.readLine();
+        System.out.print("Enter Principle: ");
+        int Principle = Integer.parseInt(br.readLine());
+
+        System.out.print("Enter annual Intrest: ");
+        float AnnualIntrest = Float.parseFloat(br.readLine());
+
+        System.out.print("Enter time in Years: ");
+        int TimeinYears = Integer.parseInt(br.readLine());
+
+        float Intrest = (Principle * AnnualIntrest * TimeinYears);
+
+        System.out.println("Custome Name: "+Name);
+        System.out.println("Principle: "+ Principle);
+        System.out.println("Annual Intrest: "+AnnualIntrest);
+        System.out.println("Time in Years: "+TimeinYears);
+        System.out.println("Intrest: "+ Intrest);
+
+    }
+
+}
